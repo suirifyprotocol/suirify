7e7843af2df1cd77ff16b86f0c9a55ea2c145012
FLOURISH <project.samclak@gmail.com>
2025-11-17 22:34:40 +0100

    // Tests

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,8 +1,8 @@
-export const PACKAGE_ID = import.meta.env.VITE_SUIRIFY_PACKAGE_ID || "0x1cc1b9ab5cf490c65e05b227910b702feb262054b4d29f1186b0757608c7dd59";
+export const PACKAGE_ID = import.meta.env.VITE_SUIRIFY_PACKAGE_ID || "0x49a5f126fac9c15657720c301d39670a4b1d679f0efead83260f2a5ccf7c726d";
 
 export const STRUCT_ATTESTATION = `${PACKAGE_ID}::protocol::Suirify_Attestation`;
 
-export const DEFAULT_EXPLORER_BASE = "https://suiexplorer.com"; // Sui explorer
+export const DEFAULT_EXPLORER_BASE = "https://suiscan.xyz/devnet/"; // Sui explorer
 
 export const explorer = {
   tx: (digest: string) => `${DEFAULT_EXPLORER_BASE}/tx/${digest}`,
